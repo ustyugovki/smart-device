2e452918489dbcdbd0db3dce5ed1a6f8efca60f3
kos <ustyugovki@bk.ru>
2023-03-13 22:47:16 +0300

    Добавил критерий About

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,8 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 
+import {aboutShow} from './modules/modals/about';
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -17,6 +19,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    aboutShow();
+
     initModals();
     const form = new Form();
     window.form = form;
