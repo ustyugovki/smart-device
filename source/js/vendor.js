80f8fc293d46d6a334f4104ad3cfbcad2fd7ae28
kos <ustyugovki@bk.ru>
2023-03-13 08:01:52 +0300

    Заверстал в Products одну карточку

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,13 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
+  },
+});
