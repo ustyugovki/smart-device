4558dff94694980157c9d644a3ee70b05acaa39e
kos <ustyugovki@bk.ru>
2023-03-13 17:50:19 +0300

    Добавил Slider в Products

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -2,12 +2,29 @@
 import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
 
-
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
   loop: true,
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    // prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next1',
+    clickable: true,
+  },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next2',
+    clickable: true,
+  },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next3',
+    clickable: true,
   },
 });
